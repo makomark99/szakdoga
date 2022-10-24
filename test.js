f94013f331b9b91a49d16b61e7bd567b38ff067b
Márk Makó <57891911+makomark99@users.noreply.github.com>
2022-10-24 20:29:18 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-function reset() {
-    document.getElementById('name').value = "";
-    document.getElementById('d1').value = "";
-    document.getElementById('d2').value = "";
-    infos=document.querySelectorAll('.info');
-    console.log(infos);
-    infos[0].outerText="";
-}
